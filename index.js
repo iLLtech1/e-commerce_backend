961128bbc17c978b920e13fca6f87763f3961093
Chris Livingston <clivingston@gmail.com>
2021-10-05 08:00:10 -0700

    commmit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const Product = require('./Product')
+const Category = require('./Category')
+const Tag = require('./Tag')
+const ProductTag = require('./ProductTag')
+
+// Products belongsTo Category
+Product.belongsTo(Category)
+// Catagories hasMany Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
+// Products belong to many Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  trough: this.ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id'
+})
+// Tags belong to many Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: this.ProductTag,
+  as: 'products',
+  foreignKey: 'tag_id' 
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag
+}
+
